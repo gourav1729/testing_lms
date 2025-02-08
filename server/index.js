4917d7bd493b098f94fa73483368da025544ba2e
Gourav <gourav.chouhan1729@gmail.com>
2025-02-08 22:12:55 +0530

    kaise ho

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
 import courseProgressRoute from "./routes/courseProgress.route.js";
+import path from "path"
 
 dotenv.config({});
 
@@ -16,6 +17,8 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+const _dirname = path.resolve(); 
 
 // default middleware
 app.use(express.json());
@@ -33,7 +36,11 @@ app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/purchase", purchaseRoute);
 app.use("/api/v1/progress", courseProgressRoute);
  
- 
+app.use(express.static(path.join(_dirname,"/client/dist")));
+app.get('*', (_,res)=>{
+    res.sendFile(path.join(_dirname,"client","dist","index.html"));
+})
+
 app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}`);
 })
